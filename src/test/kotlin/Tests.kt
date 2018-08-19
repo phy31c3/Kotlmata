7c23cdc255b910a6c341bf0584b13b063ec02d73
JongsunYoo <phy31c3@gmail.com>
2018-08-19 12:28:56 +0900

    state test 작성

--- src/test/kotlin/Tests.kt
+++ src/test/kotlin/Tests.kt
@@ -1,3 +1,4 @@
+import kr.co.plasticcity.kotlmata.Kotlmata
 import kr.co.plasticcity.kotlmata.KotlmataState
 import org.junit.Test
 
@@ -6,8 +7,38 @@ class Tests
 	@Test
 	fun stateTest()
 	{
-		KotlmataState {
-		
+		Kotlmata init {
+			debugLogger = ::println
+			errorLogger = ::error
 		}
+		
+		var initializer: KotlmataState.Initializer? = null
+		KotlmataState {
+			initializer = this
+			
+			entry action { -> println("기본 진입함수") }
+			entry via String::class action { s -> println("String타입 진입함수: $s") }
+			entry via "a" action { -> println("a 진입함수") }
+			entry via "b" action { ->
+				println("b 진입함수")
+				"next"
+			}
+			input action { _ -> println("기본 입력함수") }
+			input signal Any::class action { s -> println("Any타입 입력함수: $s") }
+			input signal "next" action { _ -> println("진입함수에서 흘러들어옴") }
+			exit action { println("퇴장함수") }
+		}.apply {
+			entry(Any()) {}
+			entry("signal") {}
+			entry("a") {}
+			entry("b") {
+				input(it)
+			}
+			input("basic")
+			input("basic", Any::class)
+			exit()
+		}
+		
+		initializer?.entry?.action { _ -> }
 	}
 }
