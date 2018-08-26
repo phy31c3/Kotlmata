59c8be6d1970eeb0f1deec2eb4f29330ebfa59d7
JongsunYoo <phy31c3@gmail.com>
2018-08-26 18:05:34 +0900

    KotlmataMachine 테스트 작성

--- src/test/kotlin/Tests.kt
+++ src/test/kotlin/Tests.kt
@@ -8,7 +8,7 @@ class Tests
 	fun init()
 	{
 		Kotlmata init {
-			debugLogger = {log -> println("Kotlmata: $log")}
+			debugLogger = { log -> println("Kotlmata: $log") }
 			errorLogger = ::error
 		}
 	}
@@ -61,12 +61,14 @@ class Tests
 			"state1" {
 				entry action { -> println("state1: 기본 진입함수") }
 				input signal String::class action { s -> println("state1: String타입 진입함수: $s") }
+				input signal "goToState2" action { -> println("state2로 이동") }
 				exit action { println("state1: 퇴장함수") }
 			}
 			
 			"state2" {
 				entry action { -> println("state2: 기본 진입함수") }
-				input signal String::class action { s -> println("state2: String타입 진입함수: $s") }
+				input signal Number::class action { s -> println("state2: Number타입 진입함수: $s") }
+				input signal 5 action { -> println("state3로 이동") }
 				exit action { println("state2: 퇴장함수") }
 			}
 			
@@ -76,11 +78,42 @@ class Tests
 				exit action { println("state3: 퇴장함수") }
 			}
 			
-			"state1" x "to state2" %= "state2"
+			"state1" x "goToState2" %= "state2"
+			"state2" x 5 %= "state3"
+			"state3" x "goToState1" %= "state1"
 			
 			initialize origin state to "state1"
 		}
 		
-		machine.input("to state2")
+		machine.input("some string")
+		machine.input("goToState2")
+		machine.input(5, Number::class)
+		machine.input(5)
+		machine.input("goToState1")
+		
+		println("-----------------------------------")
+		
+		machine {
+			has state "state1" then {
+				println("state1 있음")
+			} or {
+				println("state1 없음")
+			}
+			
+			update state "state1" set {
+				input signal String::class action { s -> println("state1: 수정된 String타입 진입함수: $s") }
+				delete action exit
+			}
+			
+			insert state "state2" of {
+				entry action { -> println("삽입된 state2") }
+			}
+			
+			insert transition ("state1" x "goToState2") %= "state3"
+			insert transition ("state1" x "goToState3") %= "state3"
+		}
+		
+		machine.input("some string")
+		machine.input("goToState2")
 	}
 }
