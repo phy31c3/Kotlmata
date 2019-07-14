7835046780420455995ceb2443b5c644a588fbdb
JongsunYoo <phy31c3@gmail.com>
2019-07-14 17:54:41 +0900

    Fallback의 Throwable 파라미터에도 힌트 달아주기 #71

--- src/main/kotlin/kr/co/plasticcity/kotlmata/keywords.kt
+++ src/main/kotlin/kr/co/plasticcity/kotlmata/keywords.kt
@@ -11,9 +11,9 @@ typealias KotlmataAction = KotlmataDSL.(signal: SIGNAL) -> Unit
 typealias KotlmataAction1<T> = KotlmataDSL.(signal: T) -> Unit
 typealias KotlmataAction2<T, R> = KotlmataDSL.(signal: T) -> R
 
-typealias KotlmataFallback = KotlmataDSL.(Throwable) -> Unit
-typealias KotlmataFallbackR<R> = KotlmataDSL.(Throwable) -> R
-typealias KotlmataFallback1<T> = KotlmataDSL.(Throwable, signal: T) -> Unit
-typealias KotlmataFallback2<T, R> = KotlmataDSL.(Throwable, signal: T) -> R
+typealias KotlmataFallback = KotlmataDSL.(throwable: Throwable) -> Unit
+typealias KotlmataFallbackR<R> = KotlmataDSL.(throwable: Throwable) -> R
+typealias KotlmataFallback1<T> = KotlmataDSL.(throwable: Throwable, signal: T) -> Unit
+typealias KotlmataFallback2<T, R> = KotlmataDSL.(throwable: Throwable, signal: T) -> R
 
 typealias KotlmataCallback = KotlmataDSL.(payload: Any?) -> Unit
